09c575f5e1cc6d065237ac6f235a94e9e5ce9c5f
Claudjos <claudjosmail@gmail.com>
2021-04-11 15:18:05 +0200

    Added settings for distribution on PyPi

--- setup.py
+++ setup.py
@@ -2,22 +2,23 @@ import sys
 from setuptools import setup, find_packages
 
 
-NAME = "cactus"
+NAME = "pycactus"
 VERSION = "0.0.1"
 
-REQUIRES = []
+REQUIRES = ["azure-functions"]
 
 
 setup(
     name=NAME,
     version=VERSION,
-    description="description",
+    description="Adapter to run an Azure Function Application with a WSGI Web Server.",
+    author="Claudjos",
     author_email="claudjosmail@gmail.com",
-    url="",
+    url="https://github.com/Claudjos/cactus",
     keywords=["Azure Function", "Web App"],
     install_requires=REQUIRES,
     packages=find_packages(),
     package_data={},
     include_package_data=False,
-    long_description="long description"
+    long_description="Adapter to run an Azure Function Application with a WSGI Web Server."
 )
